9b850da2e059bdf794315f9805901170ad1c495c
navinthomas94 <navinthomas94@gmail.com>
2022-12-04 12:22:02 -0800

    Fixes to cert.sh

--- cert/aframe/cert.sh
+++ cert/aframe/cert.sh
@@ -1,4 +1,5 @@
+gcloud container clusters get-credentials aframe-cluster
 kubectl delete secret aframe-tls 2> /dev/null
-chmod +x /home/metaverseops/project/aframe/cert-manager_install.sh && /home/metaverseops/project/aframe/cert-manager_install.sh
+chmod +x /home/metaverseops/project/aframe/git_action_requirement/cert/aframe/cert-manager_install.sh && /home/metaverseops/project/aframe/git_action_requirement/cert/aframe/cert-manager_install.sh
 export EMAIL=metaverse-serv@metaverse-363005.iam.gserviceaccount.com
-cat /home/metaverseops/project/aframe/letsencrypt-issuer.yaml | sed -e "s/email: ''/email: $EMAIL/g" | kubectl apply -f-
+cat /home/metaverseops/project/aframe/git_action_requirement/cert/aframe/letsencrypt-issuer.yaml | sed -e "s/email: ''/email: $EMAIL/g" | kubectl apply -f-
--- cert/vrspace/cert.sh
+++ cert/vrspace/cert.sh
@@ -1,4 +1,5 @@
+gcloud container clusters get-credentials vrspace-cluster
 kubectl delete secret vrspace-tls 2> /dev/null
-chmod +x /home/metaverseops/project/vrspace/cert-manager_install.sh && /home/metaverseops/project/vrspace/cert-manager_install.sh
+chmod +x /home/metaverseops/project/vrspace/git_action_requirement/cert/vrspace/cert-manager_install.sh && /home/metaverseops/project/vrspace/git_action_requirement/cert/vrspace/cert-manager_install.sh
 export EMAIL=metaverse-serv@metaverse-363005.iam.gserviceaccount.com
-cat /home/metaverseops/project/vrspace/letsencrypt-issuer.yaml | sed -e "s/email: ''/email: $EMAIL/g" | kubectl apply -f-
+cat /home/metaverseops/project/vrspace/git_action_requirement/cert/vrspace/letsencrypt-issuer.yaml | sed -e "s/email: ''/email: $EMAIL/g" | kubectl apply -f-
